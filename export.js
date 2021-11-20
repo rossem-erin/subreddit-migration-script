020934914f33c8b2955e5853180aff0d0aa3bf5c
Rossem Erin <rossem_erin@dontemailme.pls>
2021-11-20 16:23:57 +0100

    Implement basic export script

--- export.js
+++ export.js
@@ -1,1 +1,8 @@
-console.log('Sorry, this functionality is still under construction...')
+const subs = [...$$('.subscription-box li a.title')].map(el => el.text)
+
+console.log('Exporting subreddits:');
+console.log(subs.join('\n'));
+
+localStorage.setItem('autocremate-subreddits', JSON.stringify(subs));
+
+console.log(`${subs.length} Subreddits successfully stored to localStorage. You may proceed to the Import step`);
